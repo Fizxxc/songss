1f995870ded2c0acb2a79554cdc8d04fb324b23f
Fizxxc <fizzx909@gmail.com>
2025-07-06 18:41:32 +0700

    Add files via upload

--- public/firebase.js
+++ public/firebase.js
@@ -4,12 +4,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 import { getDatabase, ref, push, onValue, set } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    databaseURL: "YOUR_DB_URL",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCSKBHxE8YIDFsEW-8TpGSOHuZJ5CliIkg",
+  authDomain: "fingerpinauth.firebaseapp.com",
+  databaseURL: "https://fingerpinauth-default-rtdb.firebaseio.com",
+  projectId: "fingerpinauth",
+  storageBucket: "fingerpinauth.firebasestorage.app",
+  messagingSenderId: "395896869935",
+  appId: "1:395896869935:web:f05223a2c140bd88d662da",
+  measurementId: "G-6S5NHHLFW0"
 };
 
 const app = initializeApp(firebaseConfig);
